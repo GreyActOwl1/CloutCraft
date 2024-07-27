959587e726dfdf9b16242e746a8f710888b95ccc
Khaled Yerima <khaledyerima@Khaleds-MBP.attlocal.net>
2024-07-27 18:44:31 -0500

    Hid Sign Up/In buttons

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
           </div>
 
           {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
+          {/* <nav className="hidden md:flex md:grow"> */}
             {/* Desktop sign in links */}
-            <ul className="flex grow justify-end flex-wrap items-center">
+            {/* <ul className="flex grow justify-end flex-wrap items-center">
               <li>
                 <Link
                   href="/signin"
@@ -34,7 +34,7 @@ export default function Header() {
                 </Link>
               </li>
             </ul>
-          </nav>
+          </nav> */}
 
           <MobileMenu />
 

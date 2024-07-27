5eb45c42f3f99073e1cd704f50459d6d2aadc01b
Khaled Yerima <khaledyerima@Khaleds-MBP.attlocal.net>
2024-07-27 18:32:02 -0500

    Added in LinkedIn stock images onto FeatImage variables in zigzag.tsx under the components file

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 
-import FeatImage01 from '@/public/images/features-03-image-01.png'
-import FeatImage02 from '@/public/images/features-03-image-02.png'
-import FeatImage03 from '@/public/images/features-03-image-03.png'
+import FeatImage01 from '@/public/images/linkedin1.png'
+import FeatImage02 from '@/public/images/linkedin2.png'
+import FeatImage03 from '@/public/images/linkedin3.png'
 
 export default function Zigzag() {
   return (

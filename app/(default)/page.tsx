8475b9fabc043e4334b691f93fb6c930b9334d23
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-07-27 23:38:13 -0400

    Add Hero Text

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'CloutCraft | Supercharge Your LinkedIn Experience',
+  description: '',
 }
 
 import Hero from '@/components/hero'
--- components/features.tsx
+++ components/features.tsx
@@ -6,8 +6,8 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4">Struggling to Stand Out on LinkedIn?</h2>
+            <p className="text-xl text-gray-400">CloutCraft helps tech professionals optimize their profiles, create engaging content, and manage connections to attract the right opportunities.</p>
           </div>
 
           {/* Items */}

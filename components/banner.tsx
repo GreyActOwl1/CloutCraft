633ca81b8a7e6e90e95b32dfc1f48828583d101a
aryankumar2828 <aryan.nair.personal@gmail.com>
2024-07-28 13:04:20 +0530

    Final Changes

--- components/banner.tsx
+++ components/banner.tsx
@@ -11,6 +11,7 @@ export default function Banner() {
         <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
           <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
             <div className="text-slate-500 inline-flex">
+              {/*
               <a
                 className="font-medium hover:underline text-slate-50"
                 href="https://github.com/cruip/open-react-template"
@@ -19,9 +20,9 @@ export default function Banner() {
               >
                 Download<span className="hidden sm:inline"> on GitHub</span>
               </a>{' '}
-              <span className="italic px-1.5">or</span>{' '}
-              <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
-                Check Premium Version
+              <span className="italic px-1.5">or</span>{' '}*/}
+              <a className="font-medium hover:underline text-emerald-400" href="https://tally.so/r/m6xg7A" target="_blank" rel="noreferrer">
+                Join our Waitlist!
               </a>
             </div>
             <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>

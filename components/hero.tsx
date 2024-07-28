802227fa6c51091f2d0129f1ad124b44563accad
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-07-28 00:12:28 -0400

    Add Link to Generate Post Page

--- components/hero.tsx
+++ components/hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <Link href="/default/page" legacyBehavior>
+                <Link href="/generate-post" legacyBehavior>
                   <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0">
                   Generate a Post!
                   </a>

7d8438f239aaee66efc15f1ffbc38244a7e9771a
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-09-03 22:32:43 -0400

    feat: Add supabase client

--- next.config.js
+++ next.config.js
@@ -16,6 +16,8 @@ const nextConfig = {
     },
     env: {
         GOOGLE_AI_API_KEY: process.env.GOOGLE_AI_API_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     }
 }
 

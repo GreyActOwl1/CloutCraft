9e6047dbc8015c9ce93a175189cd6d33795e249f
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-09-07 15:57:16 +0500

    fix(api issue): Resolve api issues from this branch

--- app/(default)/api/generatePost/route.tsx
+++ app/(default)/api/generatePost/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY as string);
 
 export async function POST(req: NextRequest) {
   try {
